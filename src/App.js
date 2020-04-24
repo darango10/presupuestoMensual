f6175258698d18a2bc5c6581b15bca208220f9bc
Daniel <arangomonsalve.daniel@gmail.com>
2020-04-24 11:11:53 -0500

    Presupuesto y Formularios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Definir el state
+    const [presupuesto, guardarPresupuesto] = useState(0);
+    const [restante, guardarRestante] = useState(0);
+    const [mostrarPregunta, actualizarPregunta] = useState(true);
+
+
+    return (
+        <div className="container">
+            <header>
+                <h1>Presupuesto Mensual</h1>
+                <div className="contenido-principal contenido">
+                    {mostrarPregunta ?
+                        <Pregunta
+                            guardarPresupuesto={guardarPresupuesto}
+                            guardarRestante={guardarRestante}
+                            actualizarPregunta={actualizarPregunta}
+                        />
+                        :
+                        <div className="row">
+                            <div className="one-half column">
+                                <Formulario/>
+                            </div>
+                            <div className="one-half column">
+                                2
+                            </div>
+                        </div>}
+                </div>
+            </header>
+
+        </div>
+    );
 }
 
 export default App;
